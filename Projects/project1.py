4fbbfe5c82685d4d23224ba629c9ca38af23c513
rex7567code <lionelhuffman@seattleacademy.org>
2025-03-15 17:35:19 +0000

    project 1 submission

---
+++ Projects/project1.py
@@ -0,0 +1,25 @@
+###############################################
+### SETUP ###
+import codesters
+from codesters import StageClass
+stage = StageClass()
+###############################################
+
+stage.set_background("underwater")
+
+
+q1 = codesters.Square(100, 100 , 200,  'DeepPink')
+q2 = codesters.Square(-100, 100 , 200,  'black')
+q3 = codesters.Square(-100, -100 , 200,  'gold')
+q4 = codesters.Square(100, -100, 200, 'black')
+
+s1 = codesters.Sprite("cat3", 100, 100)
+s1.set_size(0.2)
+s2 = codesters.Sprite("draw", 98, -100)
+s2.set_size(0.5)
+s3 = codesters.Sprite("sushi", -100, 100)
+s4 = codesters.Sprite("family", -100, -100)
+s4.set_size(0.3)
+
+message1 = codesters.Text("Lionel", 0, 220,"black")
+message2 = codesters.Text("Last time i did that before again", 0, -220,"black")
