b113c74b8f89d63c81d9f66416fe037cf33bfa00
rex7567code <lionelhuffman@seattleacademy.org>
2025-03-28 17:55:16 +0000

    p2 quiz

---
+++ Projects/riddle.py
@@ -0,0 +1,10 @@
+while True:
+    word = input("What do you think grandma likes?")
+
+
+    if len(word) > 5:
+        print(f"nope")
+    else:
+        print(f"Grandma likes {word}")
+
+    print("")
