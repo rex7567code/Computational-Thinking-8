a73f330f3bb13a58ce84022aed2cdd2d0fe3738d
rex7567code <lionelhuffman@seattleacademy.org>
2025-04-23 00:52:34 +0000

    p3 revisions

--- Projects/p3_code_art.py
+++ Projects/p3_code_art.py
@@ -6,17 +6,17 @@ turtle.bgcolor("black")
 
 t = turtle.Turtle()
 t.penup()
-t.goto(-100, -50)
+t.goto(-100, 50)
 t.color("maroon")
 t.speed(10)
 t.pendown()
 
-colors = ["black","white","gray"]
+#repeating movement
 for i in range(10000):
-    t.color(  colors[ i % 3])
-    t.forward(200 + i)
-    t.left(222 + 1)
-    t.speed(10 + 10)
+    t.forward(400)
+    t.left(210 + 1)
+    t.speed(10)
+
 
 t.forward(300)
 
